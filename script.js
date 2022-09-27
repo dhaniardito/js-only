56907c55e0514ac1bf375b8baf016f67e1338daf
Dhaniardito Akbar <xdhaniardito@gmail.com>
2022-09-27 13:46:04 +0700

    cocot js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+        let kata = document.createElement('h1')
+
+        kata.textContent = "Hai her!";
+
+        document.body.append(kata);
+
+        document.write('<h1>Halo javasript!</h1>')
+
+        let body = document.getElementById('body');
+
+        body.style.backgroundColor = 'gold';
+        body.style.textAlign = 'center';
+        
